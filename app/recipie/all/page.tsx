bef5f3fa8adae5d2b85fdb2cb03487606e3ccb37
Viraj Tailor <virajtlr2006@gmail.com>
2025-11-21 20:33:51 +0530

    Feat(reciepieAction/allrecipies):Logic completed

--- app/recipie/all/page.tsx
+++ app/recipie/all/page.tsx
@@ -2,25 +2,35 @@
 
 import { allRecipiesAction } from '@/Action/RecipieAction'
 import { RecipieInsert } from '@/db/Schema'
+import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 
 const page = () => {
 
-    const [allRecipies, setallRecipies] = useState<RecipieInsert | null >(null)
+// Store all recipes in usestate(variable)
+    const [allRecipies, setallRecipies] = useState<RecipieInsert[] | null >(null)
 
     useEffect(() => {
-        all()
+        all() //Call the function everytime
     }, [])
 
     const all = async () => {
+        // Brings all recipies to frontend
         const a = await allRecipiesAction()
+        // Set all the recipies in a state
         setallRecipies(a || [])
     }
 
 
     return (
         <div>
-            All recipies here
+            {/* All recipies displayed to frontend */}
+            {allRecipies && allRecipies.map((r)=>
+            <div>
+                <img className='h-20 w-20' src={r.image || ""} alt={r.name}/>
+                <p>{r.name}</p>
+            </div>
+            )}
             
         </div>
     )
